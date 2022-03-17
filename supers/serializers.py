21e28102e92e94c3baf1ac62894a165fac78f3f5
fred <fredwright95@yahoo.com>
2022-03-17 15:32:36 -0400

    created super serializer class

---
+++ supers/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Super
+
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary ability', 'catchphrase', 'super_type']
+        
