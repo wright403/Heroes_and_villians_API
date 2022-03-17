ab4cc17be348f0f7a69724f857b9c01ba31b80d6
fred <fredwright95@yahoo.com>
2022-03-17 15:12:47 -0400

    created super and supertypes models.py

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from super_types.models import SuperTypes
+
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_type = models.ForeignKey(SuperTypes, on_delete=models.CASCADE)
+    
