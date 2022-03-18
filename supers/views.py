f3ec807b1b6a984a965357f8a1426ab50146e8f7
fred <fredwright95@yahoo.com>
2022-03-18 12:23:36 -0400

    created delete function

--- supers/views.py
+++ supers/views.py
@@ -17,7 +17,7 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
     supers = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
@@ -27,6 +27,9 @@ def supers_detail(request, pk):
         serializer = SuperSerializer(supers, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)  
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        supers.delete()
+        return Response(status.HTTP_204_NO_CONTENT)      
        
              
